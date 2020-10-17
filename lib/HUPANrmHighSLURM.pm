6c1ca1a976f8d5aaf5caa1cde7059858388a9398
Gerrit Botha <gerrit.botha@gmail.com>
2020-10-17 17:07:53 +0200

    Increased walltime to 4 days for alignment

--- lib/HUPANrmHighSLURM.pm
+++ lib/HUPANrmHighSLURM.pm
@@ -151,6 +151,7 @@ foreach my $s (@sample){
     print JOB "\#SBATCH --error=$err_file\n";               #stderr
     print JOB "\#SBATCH -n $thread_num\n";                  #thread number
     print JOB "\#SBATCH --ntasks-per-node=$thread_num\n\n";   #use the same node
+    print JOB "\#SBATCH --time=96:00:00\n"; # set time
     print JOB "$com\n";                              #commands
     close JOB;
     system("sbatch $job_file");                       #submit job
